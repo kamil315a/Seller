9e7bac1b5ebcb005f92b5f40eb9dbfc84913cb7f
kamil315 <kamilciaglo@gmail.com>
2022-08-03 19:16:13 +0200

    packages and dependency changes

--- app/src/main/java/com/example/seller/activity/MainActivity.java
+++ app/src/main/java/com/example/seller/activity/MainActivity.java
@@ -1,18 +1,13 @@
-package com.example.seller;
+package com.example.seller.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.example.seller.databinding.FragmentLoginBinding;
-import com.example.seller.ui.login.LoginFragment;
+import com.example.seller.R;
 
 public
 class MainActivity extends AppCompatActivity {
@@ -39,6 +34,7 @@ class MainActivity extends AppCompatActivity {
             public
             void onClick(View view)
                 {
+                    edtNickname.getText().toString();
 
                 }
 
