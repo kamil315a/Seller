cb2699ca42f4fd829d181e53240210f14c187652
kamil315 <kamilciaglo@gmail.com>
2022-08-03 16:33:11 +0200

    Another try to make layout better

--- app/src/main/java/com/example/seller/MainActivity.java
+++ app/src/main/java/com/example/seller/MainActivity.java
@@ -1,14 +1,64 @@
 package com.example.seller;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.seller.databinding.FragmentLoginBinding;
+import com.example.seller.ui.login.LoginFragment;
+
+public
+class MainActivity extends AppCompatActivity {
+
+    EditText edtNickname, edtEmail;
+    Button btnConfirm;
+
+    void openLoginFragment()
+        {
+
+        Fragment mFragment = null;
+        mFragment = new LoginFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.frameLayout, mFragment).commit();
+        }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected
+    void onCreate(Bundle savedInstanceState)
+        {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        edtNickname = findViewById(R.id.edtNickname);
+        edtEmail    = findViewById(R.id.edtEmail);
+        btnConfirm  = findViewById(R.id.btnConfirm);
+
+
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public
+            void onClick(View view)
+                /*{
+                try {
+                    openLoginFragment();
+                } catch (Exception e) {
+                    System.out.println("Intent Crash");
+                }
+
+                }*/
+            {
+                openLoginFragment();
+            }
+        });
+
+
+        }
 }
