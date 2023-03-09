156df675195784eaf69117ed0fb6b3abbbe181ca
kamil315 <kamilciaglo@gmail.com>
2023-03-09 14:56:03 +0100

    Commit for starting GooglePay implementation, micro changes for case_card.xml, CaseModel.java

--- app/src/main/java/com/example/seller/model/CaseModel.java
+++ app/src/main/java/com/example/seller/model/CaseModel.java
@@ -1,11 +1,15 @@
 package com.example.seller.model;
 
+import android.widget.ImageView;
+
 public
-class Case {
+class CaseModel {
 
     //Field
     Integer mCaseId;
     String  mCaseCompany;
+    ImageView mCaseCompanyLogo; // hard to tell what should i do with ImageView.
+                                // The image should be load from local disk or from MYSQL DB?
     String  mCaseName;
     String  mCaseDescription;
     Integer mCounter;
@@ -13,7 +17,7 @@ class Case {
 
 
     public
-    Case(Integer mCaseId , String mCaseCompany , String mCaseName , String mCaseDescription , Integer mCounter)
+    CaseModel(Integer mCaseId , String mCaseCompany , String mCaseName , String mCaseDescription , Integer mCounter)
         {
         this.mCaseId          = mCaseId;
         this.mCaseCompany     = mCaseCompany;
